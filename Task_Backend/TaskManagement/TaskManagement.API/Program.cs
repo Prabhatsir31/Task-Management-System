3c20b522b506e1fc1f9417347b18637dafc1a88d
Prabhat Srivastava <srivastavaprabhat522@gmail.com>
2026-02-20 09:00:00 +0000

    backend

---
+++ Task_Backend/TaskManagement/TaskManagement.API/Program.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+namespace TaskManagement.API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+               //.ConfigureServices((hostContext, services) =>
+               // {
+               //     UTRegisterQuartzExtension.RegisterUTQuartzServices(hostContext, services);
+               // });
+    }
+}
