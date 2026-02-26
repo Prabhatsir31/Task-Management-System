df3c0a60ab8b7686bbea0834977ea3ebf475afcd
Prabhat Srivastava <srivastavaprabhat522@gmail.com>
2026-02-26 09:00:00 +0000

    backend

---
+++ Task_Backend/TaskManagement/TaskManagement.Business/Interfaces/IUserRoleAccessManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using TaskManagement.Domain.DTOs;
+using TaskManagement.Domain.ResponseModel;
+
+namespace TaskManagement.Business.Interfaces
+{
+    public interface IUserRoleAccessManager
+    {
+        Task<UserRoleAccessDto> GetUserAccessOnRoleAsync(int roleId);
+        Task<List<AllUserRoleAccessDto>> GetAllUserAccessMappingAsync();
+        Task<ResultModel> AssingUserRoleAccessAsync(UserRoleAccessDto userRoleAccessDto);
+    }
+}
